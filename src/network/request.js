e7586c43f9e603ef6ce1d2aae990561bbc0a3d10
fc <1344623045@qq.com>
2019-11-25 14:49:45 +0800

    '首页请求数据'

---
+++ src/network/request.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+
+export default function request(config) {
+  const instance = axios.create({
+    baseURL: 'http://106.54.54.237:8000/api/v1',
+    timeout: 5000
+  });
+  instance.interceptors.request.use(config => {
+    return config
+  }, err => {
+    console.log(err)
+  })
+  instance.interceptors.response.use(res => {
+    return res.data
+  }, err => {
+    console.log(err)
+  })
+  return instance(config)
+}
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,24 +1,38 @@
 <template>
   <div>
-    <nav-bar>
+    <nav-bar class="home-nav">
       <div slot="center">购物街</div>
     </nav-bar>
   </div>
 </template>
 <script>
-import NavBar from "components/common/navbar/NavBar";
+import NavBar from 'components/common/navbar/NavBar'
+import { getHomeMultidata } from 'network/home'
 export default {
-  name: "Home",
+  name: 'Home',
   data() {
-    return {};
+    return {
+      banners: [],
+      recommends: []
+    }
+  },
+  created() {
+    //  请求多个数据
+    getHomeMultidata().then(res => {
+      const { banner, recommend } = res.data
+      this.banners = banner.list
+      this.recommends = recommend.list
+    })
   },
   components: {
     NavBar
   }
-};
+}
 </script>
 <style>
-nav-bar {
+.home-nav {
   text-align: center;
+  background-color: var(--color-tint);
+  color: #fff;
 }
 </style>
